17db24931d5181d849279db160fed7c72ea571e9
MKzeNova <keyboardwin3@gmail.com>
2025-11-01 10:20:56 +0530

    Update qn27.py

--- Basic problems/qn27.py
+++ Basic problems/qn27.py
@@ -2,10 +2,10 @@
 #The number must be divisible by five
 #If the number is greater than 150, then skip it and move to the following number
 #If the number is greater than 500, then stop the loop
-list=[12, 75, 150, 180, 145, 525, 50]
+list=[12, 75, 150, 180, 145, 525, 50] .
 for i in list:
     if i>500:
-        break
+        break #break stops the entire loop immediately.The loop ends here no further numbers are checked.
     elif i>150:
         continue  #Meaning of continue:It skips the rest of the code inside the loop for this iteration and moves to the next number.
         print("hI")
@@ -15,3 +15,4 @@ for i in list:
 #output 75 150 145
 
      
+
