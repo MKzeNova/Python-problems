c1981699f90871088abd6d96afbe80fe5f6ccb56
MKzeNova <keyboardwin3@gmail.com>
2025-11-01 10:12:39 +0530

    Add files via upload

---
+++ Basic problems/qn27.py
@@ -0,0 +1,16 @@
+#  Python program to display only those numbers from a list that satisfy the following conditions
+#The number must be divisible by five
+#If the number is greater than 150, then skip it and move to the following number
+#If the number is greater than 500, then stop the loop
+list=[12, 75, 150, 180, 145, 525, 50]
+for i in list:
+    if i>500:
+        break
+    elif i>150:
+        continue
+        print("hI")
+    elif i%5==0:
+        print(i)
+
+#output 75 150 145
+     
