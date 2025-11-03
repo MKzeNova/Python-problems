bfd19370a62020102413cadd48987e1264e5335a
MKzeNova <keyboardwin3@gmail.com>
2025-11-03 19:05:42 +0530

    Update qn31.py

--- Basic problems/qn31.py
+++ Basic problems/qn31.py
@@ -1,7 +1,9 @@
+#Reverse a integer number
 n=int(input("Enter your number: "))
 reversed_no=0
 while n >0:
     a=n%10
     reversed_no=reversed_no*10 +a #if there is no '*10' it just gives sum of digits
     n=n//10
+
 print("Reversed number: ",reversed_no)
