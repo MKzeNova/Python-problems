3a525a333dfe35b99b0038e6f208d279fb5b4ce9
MKzeNova <keyboardwin3@gmail.com>
2025-11-03 19:04:24 +0530

    Update qn30.py

--- Basic problems/qn30.py
+++ Basic problems/qn30.py
@@ -1,3 +1,4 @@
+#factorial of a given number using for loop
 n=int(input("Enter your number:"))
 factorial=1
 if n==0:
@@ -7,4 +8,5 @@ elif n<0:
 else:
     for i in range(1,n+1):
         factorial*=i
+
     print("factorial of the number", factorial)
