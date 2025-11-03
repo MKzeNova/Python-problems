fb46d9e1a151ffbcf7fa966506bab01213f1e553
MKzeNova <keyboardwin3@gmail.com>
2025-11-03 19:02:44 +0530

    Update qn28.py

--- Basic problems/qn28.py
+++ Basic problems/qn28.py
@@ -1,3 +1,4 @@
+#prime numbers within a range eg b/w 25 and 50
 for i in range(25,51):
     if i >1:
         for num in range(2,i):
@@ -5,3 +6,4 @@ for i in range(25,51):
                 break # if this condition become false after checking every numbers from 2 to i-1 , the code will execute from outside of the "if".
         else:
             print(i)
+
