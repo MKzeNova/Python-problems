6cb0bef4a33191b91189798b207fef6c58908740
MKzeNova <keyboardwin3@gmail.com>
2025-12-12 19:21:01 +0530

    Update qn01.py

--- Basic problems/qn01.py
+++ Basic problems/qn01.py
@@ -1,6 +1,9 @@
+# write a programme to calculate the area of a circle of radius r entered by user
+
 r=float(input("Enter the radius="))
 import math
 if r==0:
     print("enter a non zero value")
 else:
+
     print("Area of the given circle=",math.pi*r*r)
