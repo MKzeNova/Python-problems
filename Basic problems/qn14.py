9964455076cd1f96830f1551fd951c75dfefdac2
MKzeNova <keyboardwin3@gmail.com>
2025-12-17 10:59:57 +0530

    Update qn14.py

--- Basic problems/qn14.py
+++ Basic problems/qn14.py
@@ -1,5 +1,6 @@
 #program that displays your name, age, and address on three different lines.
 def data():
-    name, age,adress="sinan",19,"kerala" 
+    name, age,adress="sinan",99,"kerala" 
     print("name:{}\nage:{}\naddress:{}".format(name,age,adress))   #curly braces {} are placeholders for the values that will be passed through the .format() method.
 data()
+
